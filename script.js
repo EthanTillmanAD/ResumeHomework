ccb9e36a44badc1094de80181edc02320d422e06
EthanTillmanAD <ethantillman101@gmail.com>
2024-11-18 23:47:35 -0500

    new resume

---
+++ script.js
@@ -0,0 +1,27 @@
+setInterval(time, 1000);
+function time(){
+    const date  = new Date();
+
+
+    let hours = date.getHours();
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+
+    hours = hours ? hours :12;
+    document.getElementById("clock").innerText = `${hours}:${minutes}:${seconds}  ${ampm}`;
+
+}
+function todaysDate(){
+    const date = new Date();
+
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    document.getElementById("date").innerText = `${month}, ${day}, ${year}`;
+}
+window.onload = todaysDate
+
